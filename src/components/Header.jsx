a18ac545d52a379d84dc49bca3321c98ec42188c
Yefersn15 <yefersonandres225@gmail.com>
2025-09-01 01:10:40 -0500

    Configuración para GitHub Pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,24 @@
 // Header.jsx
-import { useNavigate } from "react-router-dom";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
 import { useTheme } from "./ThemeContext";
 import ToggleTheme from "./ToggleTheme";
-
 const Header = () => {
   const { themeClass } = useTheme();
-  const navigate = useNavigate();
-  const userData = JSON.parse(localStorage.getItem("currentUser") || "null");
-  
-  const handleLogout = () => {
-    localStorage.removeItem("currentUser");
-    navigate("/");
-  };
   
   return (
-    <header className={`${themeClass} p-3`}>
+    <header className={`${themeClass} p-4`}>
       <div className="container">
         <div className="d-flex align-items-center justify-content-between">
+          <a href="https://vite.dev" target="_blank" rel="noreferrer">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <h1 className="mb-0 text-center flex-grow-1">Sistema de Parqueadero</h1>
+          <ToggleTheme/>
           <div className="d-flex align-items-center">
-            <i className="bi bi-p-circle fs-2 text-warning me-2"></i>
-            <h1 className="mb-0 fs-4">Sistema de Parqueadero</h1>
-          </div>
-          
-          <div className="d-flex align-items-center">
-            {userData && (
-              <>
-                <span className="me-3 d-none d-md-block">
-                  Bienvenido, <strong>{userData.nombre}</strong> 
-                  <span className={`badge ${userData.tipo_usuario === 'administrador' ? 'bg-danger' : userData.tipo_usuario === 'operador' ? 'bg-warning' : 'bg-success'} ms-2`}>
-                    {userData.tipo_usuario}
-                  </span>
-                </span>
-                <button 
-                  className="btn btn-outline-light btn-sm me-2"
-                  onClick={() => navigate("/DashBord")}
-                >
-                  <i className="bi bi-speedometer2 me-1"></i>Dashboard
-                </button>
-                <button 
-                  className="btn btn-outline-light btn-sm me-2"
-                  onClick={handleLogout}
-                >
-                  <i className="bi bi-box-arrow-right me-1"></i>Salir
-                </button>
-              </>
-            )}
-            <ToggleTheme/>
+            <a href="https://react.dev" target="_blank" rel="noreferrer">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
           </div>
         </div>
       </div>
