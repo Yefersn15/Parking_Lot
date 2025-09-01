c46e28fd62c047ac5a4c8d2341b5ce5f19f5160b
Yefersn15 <yefersonandres225@gmail.com>
2025-09-01 01:08:26 -0500

    Configuración para GitHub Pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+// Footer.jsx
 import { Link } from "react-router-dom";
 import { useTheme } from "./ThemeContext";
 
@@ -5,45 +6,41 @@ const Footer = () => {
   const { themeClass } = useTheme();
 
   return (
-    <footer className={`${themeClass} mt-4 pt-5`}>
+    <footer className={`${themeClass} mt-4 pt-4`}>
       <div className="container">
         <div className="row text-center text-md-start">
           <div className="col-md-4 mb-4">
             <h5>Sobre Nosotros</h5>
             <p>
-              Proyecto Three es un portal educativo en React donde exploramos
-              conceptos clave como Props, State, Hooks, Redux y más.
+              Sistema de Gestión de Parqueadero - Una solución moderna para 
+              administrar espacios de estacionamiento de manera eficiente.
             </p>
           </div>
 
           <div className="col-md-4 mb-4">
-            <h5>Páginas</h5>
+            <h5>Accesos Rápidos</h5>
             <ul className="list-unstyled">
-              <li><Link className="text-white text-decoration-none" to="/Props">Props</Link></li>
-              <li><Link className="text-white text-decoration-none" to="/State">State</Link></li>
-              <li><Link className="text-white text-decoration-none" to="/LifeCycle">Ciclo de Vida</Link></li>
-              <li><Link className="text-white text-decoration-none" to="/Hooks">Hooks</Link></li>
-              <li><Link className="text-white text-decoration-none" to="/VirtualDOM">Virtual DOM</Link></li>
-              <li><Link className="text-white text-decoration-none" to="/Redux">Redux</Link></li>
+              <li><Link className="text-white text-decoration-none" to="/DashBord">Dashboard</Link></li>
+              <li><Link className="text-white text-decoration-none" to="/Parking">Ver Parqueadero</Link></li>
+              <li><Link className="text-white text-decoration-none" to="/gestion-vehiculos">Mis Vehículos</Link></li>
             </ul>
           </div>
 
           <div className="col-md-4 mb-4">
-            <h5>Síguenos</h5>
-            <a href="https://facebook.com" target="_blank" className="text-white me-3">
-              <i className="bi bi-facebook"></i> Facebook
-            </a><br />
-            <a href="https://twitter.com" target="_blank" className="text-white me-3">
-              <i className="bi bi-twitter"></i> Twitter
-            </a><br />
-            <a href="https://instagram.com" target="_blank" className="text-white">
-              <i className="bi bi-instagram"></i> Instagram
-            </a>
+            <h5>Soporte</h5>
+            <p>
+              <i className="bi bi-envelope me-2"></i>
+              soporte@parqueadero.com
+            </p>
+            <p>
+              <i className="bi bi-telephone me-2"></i>
+              +57 1 234 5678
+            </p>
           </div>
         </div>
 
         <div className="text-center border-top border-secondary py-3 mt-3">
-          © {new Date().getFullYear()} Proyecto Three | Todos los derechos reservados
+          © {new Date().getFullYear()} Sistema de Parqueadero | Todos los derechos reservados
         </div>
       </div>
     </footer>
